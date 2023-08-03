0d6df03cf230dc2268e6aafd816f8326f499ab2b
kengurenokRu <nastassiadunaeva@gmail.com>
2023-08-03 17:17:58 +0300

    fix: task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let str = [];
+    for (let i=0; i<expr.length; i+=10)
+       str.push(expr.slice(i, i + 10));    
+    for (let i=0; i<str.length; i++)
+    {
+        str[i] = str[i].replace(/10/g, ".");
+        str[i] = str[i].replace(/11/g, "-");
+        str[i] = str[i].replace(/0/g, "");
+    }
+    let result = MORSE_TABLE[str[0]];
+    for (let i=1; i<str.length; i++)
+    {
+        if (MORSE_TABLE[str[i]] == undefined)
+        result = result +" ";
+        else 
+        result = result +MORSE_TABLE[str[i]];        
+    }    
+    return result;
 }
 
 module.exports = {
